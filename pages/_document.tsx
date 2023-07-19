3e0c5ac22780033a298dee373b49a6eb34fbcdf1
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2023-07-19 13:28:32 -0500

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,10 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png"></link>
         <meta name="theme-color" content="#fff" />
+        <meta name="description" content="Chillin' Buds Inventory Product Search Engine" />
+        <meta name="keywords" content="CBD, Search, Engine, Chillin' Buds, Chillin Buds, ChillinBuds, Chillin' Buds CBD, Chillin Buds CBD, ChillinBuds CBD, Chillin' Buds Search, Chillin Buds Search, ChillinBuds Search, Chillin' Buds CBD Search, Chillin Buds CBD Search, ChillinBuds CBD Search, Chillin' Buds CBD Search Engine, Chillin Buds CBD Search Engine, ChillinBuds CBD Search Engine" />
+        <meta name="author" content="Chillin' Buds" />  
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <body>
         <Main />
